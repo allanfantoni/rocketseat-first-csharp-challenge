3deae2169c17a4af23043abb69eb14068ed33fa1
Allan Fantoni <allan.fantoni@outlook.com>
2025-05-29 00:44:47 -0300

    feat: create a project that returns a welcome message to the user

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# rocketseat-first-csharp-challenge
-Rocketseat's First C# Challenge
+# Rocketseat's First C# Challenge
+
+First practical exercises to master C#, provided by Rocketseat's C# training course.
+
+## Projects
+
+- 1. WelcomeMessage: Create a program in which the user needs to enter a name and a personalized welcome message with his name. Example: `Olá, Welisson! Seja muito bem-vindo!`
+
+## .NET and C# version
+
+- .NET 9.0
+- C# 13.0
---
+++ RocketseatFirstCSharpChallenge/WelcomeMessage/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace WelcomeMessage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome:");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Olá, {name}! Seja muito bem-vindo");
+
+            Console.WriteLine("Press any key to close this window . . .");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
